3e492f62e8e02612cfcd748fe2d281c65faeded9
Draconiix23 <Kyla.heighes@umontana.edu>
2025-02-10 18:54:57 -0700

    Added moving functions, Variables, and a Lightswitch
    you.. have no idea how long it took to make the light work lmao

---
+++ 2_Homework/sketch.js
@@ -0,0 +1,170 @@
+//VARIABLES//
+ let isVisible = true;
+ var OpacLight = 25;
+  //-------Here's your Rangoon Variables m'lord-----//
+    //first crabrangoon//
+    var oneCTx = 210;
+    var oneCTy = 330;
+    var oneCLx = 200;
+    var oneCLy = 400;
+    var oneCRx = 370;
+    var oneCRy = 400;
+
+    var oneDiry = 0.2;
+    
+    //second crabrangoon//
+    var secCTx = 280;
+    var secCTy = 310;
+    var secCLx = 240;
+    var secCLy = 370;
+    var secCRx = 430;
+    var secCRy = 400;
+    
+    var secDiry = 0.2;
+    
+    //third crabrangoon//
+    var thiCTx = 400;
+    var thiCTy = 300;
+    var thiCLx = 300;
+    var thiCLy = 350;
+    var thiCRx = 470;
+    var thiCRy = 400;
+
+    var thiDiry = 0.2;
+
+  //--------------------------------------------//
+
+//SETUP//
+function setup() {
+  createCanvas(800, 725);
+}
+//DRAW HERE//
+function draw() {
+  background(50);
+  stroke(80,20,20)
+  strokeWeight(1)
+  table()
+  lightswitch()
+  plate()
+  crabgoon()
+  sauce()
+  saucespecks()
+  Light()
+  nameandtitle()
+}
+
+//FUNCTIONS//
+function table(){
+  fill(120,80,0)
+  rect(0,500,50,220)
+  rect(750,500,50,220)
+  fill(150,100,0)
+  rect(0,320,800,220)
+}
+function lightswitch(){
+  fill(220,220,220)
+  rect(60,100,100,130)
+  fill(240,240,240)
+  rect(72,110,75,110)
+  rect(95,145,25,40)
+  fill(220,220,220)
+  circle(107,130,10)
+  circle(107,200,10)
+  
+  if (isVisible){
+    fill(250,250,250)
+    square(100,150,15)
+  }
+  else{
+    fill(250,250,250)
+    square(100,165,15)   
+   }
+     
+}
+function plate(){
+  fill(500,500,500)
+  ellipse(400,450,390,100)
+  ellipse(400,400,500,160)
+  fill(230,230,230)
+  ellipse(400,400,390,100)
+}
+function crabgoon(){
+  fill(200,130,0)
+  
+  //third crabrangoon//
+  triangle(thiCTx,thiCTy,thiCLx,thiCLy,thiCRx,thiCRy)
+    thiCTy += thiDiry;
+    thiCLy += thiDiry;
+    thiCRy += thiDiry;
+    if(thiCTy >= 310 || thiCTy <= 300)
+       {
+        thiDiry *=  -1;
+       }
+  
+  //second crabrangoon//
+  triangle(secCTx,secCTy,secCLx,secCLy,secCRx,secCRy)
+    secCTy += secDiry;
+    secCLy += secDiry;
+    secCRy += secDiry;
+    if(secCTy >= 320 || secCTy <= 310)
+       {
+        secDiry *=  -1;
+       }
+  
+  //first crabrangoon//
+  triangle(oneCTx,oneCTy,oneCLx,oneCLy,oneCRx,oneCRy)
+    oneCTy += oneDiry;
+    oneCLy += oneDiry;
+    oneCRy += oneDiry;
+    if(oneCTy >= 340 || oneCTy <= 330)
+       {
+        oneDiry *=  -1;
+       }
+}
+function sauce(){
+  fill (245,300,300)
+  rect(475,370,100,50)
+  rect(470,370,110,15)
+  
+  fill (170,0,0,200)
+  rect(475,400,100,20)
+}
+function saucespecks(){
+  fill(250,100,0,200)
+  square(500,405,5)
+  square(550,410,5)
+  
+  fill(100,0,0,200)
+  square(525,407,7)
+  square(480,410,6)
+  square(563,403,5)
+}
+function nameandtitle(){
+  fill(200,200,200)
+  stroke(50,50,50)
+  strokeWeight(3)
+  textSize(34)
+  text('Kyla-Ann Heighes',515,700)
+  
+  fill(300,300,300)
+  stroke(150,100,100)
+  strokeWeight(3)
+  textSize(36)
+  text('Introducing... CrabRangoons',10,50)
+}
+
+function Light(){
+  
+  if(isVisible){
+    strokeWeight(0)
+    fill(250,220,0,OpacLight)
+    triangle(400,-50,150,400,640,400)
+    ellipse(400,430,600,200)
+  }
+
+}
+//INTERACTIVITY FUNCTIONS//
+
+function mousePressed(){
+  isVisible = !isVisible // toggle light on or off
+}
